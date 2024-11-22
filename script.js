218c00a92d354f6e1dceabe75664adf24320234d
Raziy8685 <khodabandehraziy@gmail.com>
2024-11-22 13:29:09 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('signup-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('ثبت‌نام با موفقیت انجام شد!');
+    window.location.href = 'login.html';
+});
